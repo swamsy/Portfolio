aac4026c497fab68eb0c46876768edbfa2d19fab
Michael Swan <97557851+swamsy@users.noreply.github.com>
2023-06-25 15:20:54 -0400

    initial commit

---
+++ functions/contact.js
@@ -0,0 +1,65 @@
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
+require('dotenv').config();
+
+let accessToken;
+const tokenGenerator = xoauth2.createXOAuth2Generator({
+    user: process.env.GMAIL_USERNAME,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENTSECRET,
+    refreshToken: process.env.OAUTH_REFRESHTOKEN
+});
+
+tokenGenerator.on('token', token => {
+    accessToken = token.accessToken;
+});
+
+tokenGenerator.getToken((err, token) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    accessToken = token;
+});
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.GMAIL_USERNAME,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENTSECRET,
+        refreshToken: process.env.OAUTH_REFRESHTOKEN,
+        accessToken: accessToken
+    }
+});
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+
+    const body = JSON.parse(event.body);
+
+    const mailOptions = {
+        from: body.email,
+        to: process.env.GMAIL_USERNAME,
+        subject: `New Message From ${body.formName}`,
+        text: `Name: ${body.formName}\nEmail: ${body.email}\nMessage: ${body.message}`,
+    }
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ status: 'success' }),
+        };
+      } catch (error) {
+        console.error(error);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ status: 'error' }),
+        };
+      }
+    };
---
+++ public/app.js
@@ -0,0 +1,70 @@
+// Contact form functionality
+const contactForm = document.querySelector('.contact-form');
+const notification = document.getElementById('notification');
+const notificationMessage = document.getElementById('notification-message');
+
+let formName = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let formData = {
+        formName: formName.value,
+        email: email.value,
+        message: message.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/.netlify/functions/contact');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function(){
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success'){
+            notification.classList.remove('hide');
+            notification.style.backgroundColor = '#00C851';
+            notificationMessage.innerHTML = 'Email sent!<i class="fa-solid fa-circle-check"></i>';
+            formName.value = '';
+            email.value = '';
+            message.value = '';
+        }else{
+            notification.classList.remove('hide');
+            notification.style.backgroundColor = '#ff4444';
+            notificationMessage.innerHTML = 'Something went wrong!<i class="fa-solid fa-circle-xmark"></i>';
+        }
+
+        setTimeout(() => {notification.classList.add('hide');}, 4000);
+    }
+
+    xhr.send(JSON.stringify(formData));
+
+});
+
+
+// Scroll to top button functionality
+const sttButton = document.querySelector('.stt');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 50) {
+        sttButton.classList.add('visible');
+    } else {
+        sttButton.classList.remove('visible');
+    }
+});
+
+
+// Hamburger menu functionality
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+document.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
